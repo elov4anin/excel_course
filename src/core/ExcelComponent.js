c69261b8c2cf47781f32c23e104e2c3726160902
Sergey Chugaev <elov4anin@gmail.com>
2020-09-20 23:41:13 +0500

    structure, html, components

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,10 @@
+import {DomListener} from '@core/DomListener'
+
+export class ExcelComponent extends DomListener {
+    /**
+     * Возращает шаблон копмонента
+     * @return {string}
+     */
+    toHTML() {
+    }
+}
